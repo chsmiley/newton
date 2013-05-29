1685b8c7101bfacf4439d83682cbb965fc43efe7
Charese Smiley <chsmiley@indiana.edu>
2013-05-29 11:31:16 -0400

    small changes

--- sent_tokenize.py
+++ sent_tokenize.py
@@ -1,10 +1,9 @@
 # This program tokenizes a file into sentences using an abbreviation list and a rule-based approach
+# Customized for the Isaac Newton corpus which contains a mixture of English, Latin, and Frech, many non-English abbreviations, and a number of irregularities
 
 import sys, re
 
 detect_abbr = re.compile(r"((( |^)((([^\s]+\.|etc)( |$)+){2,}|([Pp](ag)?|[Cc]h|[Tt]h|[Vv](ol)?|ib|et|in)\.?( |$)+)+([0-9]+\.?,? *)*)+|i\. +e\.)") # any sequence of 2 or more words ending in a period or one of a short list of section abbreviaions (e.g. page, chapter, etc.) optionally followed by a number  
-
-# detect_abbr = re.compile(r"((( |^)(([a-zA-Z]+\.( |$)+){2,}|([Pp](ag)?|[Cc]h|[Tt]h|[Vv](ol)?|ib)\.?( |$)+)+([0-9]+\.?,? *)*)+|i\. +e\.)") # any sequence of 2 or more words ending in a period or one of a short list of section abbreviaions (e.g. page, chapter, etc.) optionally followed by a number  
 
 # load a file and strip off return characters from the end of each line
 def load_file(filename):
@@ -58,12 +57,12 @@ def segment_sent(essay):
                    if re.search("p(ag)?\.? +([0-9]+,? *)+\. *###",word): # look for page numbers and remove marker
                        word = word[:-3].strip()
                        currentline.append(word)
-                   else:
+                   else: #other sequences of abbreviations
                        currentline.append(word[:-3].strip())
-                else:
+                else: 
                     currentline.append(word)
                 if re.search("\.$",word):
-                    if not (check_abbr(abbrlist,word) or check_abbr(abbrlist,word[:-1])):
+                    if not (check_abbr(abbrlist,word) or check_abbr(abbrlist,word[:-1])): # the word is not in the abbreviation list with or without a final period
                         if re.search("^[0-9]*\.?$", currentline[-1]) and len(currentline) > 1 and not re.search("(,$|^p(ag)?$)", currentline[-2]) and not (currentline[-1][:-1].isdigit() and currentline[-2].isdigit() and not (int(currentline[-1][:-1]) - int(currentline[-2]) > 1)):
                             sentences.append(currentline[:-1]) #fix this
                             currentline = currentline[-1]
@@ -84,32 +83,6 @@ def second_pass(sentences):
                 new_sentences[count-1] + sentence
 
     return new_sentences
-
-# segment the file into sentences using the abbreviation list
-#def segment_sent(essay):
-#    currentline = []
-#    sentences = []
-#    abbrlist = load_file("abbr_list.txt")
-#    for line in essay:
-#        newline = []
-#        line = re.sub(" +\.",".",line)
-#        revisedline = revise_line(newline,line)
-#        if len(revisedline) > 0:
-#            for word in revisedline:
-#                if re.search("###$",word):
-#                    currentline.append(word[:-3].strip())
-#                    if re.search("p(ag)? +([0-9],?)+\.?###",word):
-#                        sentences.append(currentline)
-#                        currentline = []
-#                        print word
-#                else:
-#                    currentline.append(word.strip())
-#                if re.search(".*\.$",word):
-#                    if not check_abbr(abbrlist,word) and not check_abbr(abbrlist,word[:-1]) and not re.match("(etc\.?|in|et)",word):
-##                if not ((len(currentline) == 2) and re.match("[0-9]+",currentline[0]) and (currentline[1] == "."))or not ((len(currentline) == 1) and (currentline == "e.")): #ignore sentences of length 2 in the format # . as they are probably part of a list
-#                        sentences.append(currentline)
-#                        currentline = []
-#    return sentences
 
 def print_doc(filename,sentences):
     basefilename = filename.split(".")[0]
